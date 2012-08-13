fa80d21d573ca2a349e9b23a5bc79f5e67bcf5f8
Edouard DUPIN <yui.heero@gmail.com>
2012-08-13 09:49:07 +0200

    add tag version on this parser

--- Linux.mk
+++ Linux.mk
@@ -5,6 +5,11 @@ include $(CLEAR_VARS)
 # name of the librairy
 LOCAL_MODULE := parsersvg
 
+# get the tag of the current project : 
+LOCAL_VERSION_TAG=$(shell cd $(LOCAL_PATH) ; git describe --tags)
+LOCAL_VERSION_TAG_SHORT=$(shell cd $(LOCAL_PATH) ; git describe --tags --abbrev=0)
+$(info $(LOCAL_MODULE) version TAG : $(LOCAL_VERSION_TAG))
+
 # name of the dependency
 LOCAL_STATIC_LIBRARIES := etk agg tinyxml
 
@@ -12,7 +17,7 @@ LOCAL_C_INCLUDES :=
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
-LOCAL_CFLAGS := -DPARSER_SVG_VERSION_TAG_NAME="\"???-$(BUILD_DIRECTORY_MODE)\""
+LOCAL_CFLAGS := -DPARSER_SVG_VERSION_TAG_NAME="\"$(LOCAL_VERSION_TAG_SHORT)-$(BUILD_DIRECTORY_MODE)\""
 
 
 # load the common sources file of the platform
