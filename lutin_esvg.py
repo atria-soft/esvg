ac506182726f44783d8035d74b21895dcd048c53
Edouard DUPIN <yui.heero@gmail.com>
2013-12-22 19:02:20 +0100

    [DEV] add monk documentation

--- lutin_esvg.py
+++ lutin_esvg.py
@@ -4,11 +4,6 @@ import lutinTools
 
 def Create(target):
 	myModule = lutinModule.module(__file__, 'esvg', 'LIBRARY')
-	# enable doculentation :
-	myModule.doc_enable()
-	myModule.documentation.set_website("http://HeeroYui.github.io/esvg/")
-	myModule.documentation.set_path(lutinTools.GetCurrentPath(__file__) + "/esvg/")
-	myModule.documentation.set_external_link(['etk', 'agg', 'exml'])
 	
 	myModule.AddModuleDepend(['etk', 'agg', 'exml'])
 	
---
+++ monk_esvg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import monkModule as module
+import monkTools as tools
+
+def create():
+	# module name is 'edn' and type binary.
+	myModule = module.Module(__file__, 'esvg', 'LIBRARY')
+	# enable doculentation :
+	myModule.set_website("http://heeroyui.github.io/esvg/")
+	myModule.set_path(tools.get_current_path(__file__) + "/esvg/")
+	# add the currrent module at the 
+	return myModule
+
+def get_desc():
+	return ""
