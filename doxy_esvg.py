67a6da609961eb10ed828eb914004a6ce94fe1d7
Edouard DUPIN <yui.heero@gmail.com>
2016-03-30 21:40:01 +0200

    [DEV] use doxygen documentation

---
+++ doxy_esvg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import os
+import doxy.module as module
+import doxy.debug as debug
+import doxy.tools as tools
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name)
+	my_module.set_version([0,7,"dev"])
+	my_module.set_title("esvg: Ewol SVG parser and renderer")
+	my_module.set_website("http://atria-soft.github.io/" + module_name)
+	my_module.set_website_sources("http://github.com/atria-soft/" + module_name)
+	my_module.set_path(os.path.join(tools.get_current_path(__file__), module_name))
+	my_module.add_module_depend([
+	    'exml',
+	    'etk'
+	    ])
+	
+	return my_module
